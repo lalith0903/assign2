d6b3ba288f196797b1e07e871730ac190327f6de
lalith0903 <76272359+lalith0903@users.noreply.github.com>
2022-01-24 23:59:06 +0530

    Add files via upload

---
+++ assign2.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from BeautifulSoup import BeautifulSoup
+import pandas as pd
+
+driver = webdriver.Chrome("/usr/lib/chromium-browser/chromedriver")
+
+products=[] #List to store name of the product
+prices=[] #List to store price of the product
+features=[] #List to store rating of the product
+driver.get("<a href="https://www.flipkart.com/search?q=mobiles&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=on&as=off">)
+
+content = driver.page_source
+soup = BeautifulSoup(content)
+for a in soup.findAll('a',href=True, attrs={'class':'_4rR01T'}):
+name=a.find('div', attrs={'class':'_4rR01T'})
+price=a.find('div', attrs={'class':'_30jeq3 _1_WHN1'})
+feature=a.find('div', attrs={'class':'fMghEO'})
+products.append(name.text)
+prices.append(price.text)
+features.append(feature.text)
